bd4eb5dab255cabf525c827e00b88b9500f9587f
Sumit Godhaniya <godhaniyasumit5@gmail.com>
2026-08-03 23:35:52 -0700

    add: create new python1.py file

---
+++ python2.py
@@ -0,0 +1,33 @@
+# 2nd python program
+a = None
+print(type(a))
+
+a = 10
+print(type(a))
+
+a = 10.20
+print(type(a))
+
+a = "ksc"
+print(type(a))
+
+a = 3.5+2j
+print(type(a))
+
+a = True
+print(type(a))
+
+a = [1, 2, 3, 4, 5]
+print(type(a))
+
+b = bytes(a)
+print(type(b))
+
+b = bytearray(a)
+print(type(b))
+
+a = [1, 2, 3, 4, 5]
+print(type(a))
+
+a = (1, 2, 3, 4, 5)
+print(type(a))

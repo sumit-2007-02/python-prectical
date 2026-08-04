423f50592bd1aca1a67ede4e8a0c0c8de7934721
Sumit Godhaniya <godhaniyasumit5@gmail.com>
2026-08-04 00:32:36 -0700

    program 4 th complete

---
+++ python4.py
@@ -0,0 +1,16 @@
+a = "10"
+b = "10.5"
+d = 50
+c = 0   
+
+w = int(a)
+print("String to Integer:",w)
+
+x = float(b)
+print("String to Float:",x)
+
+y = str(d)
+print("Integer to String:",y)
+
+z = bool(c)
+print("Float to String:",z)
